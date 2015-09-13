60e23af0092161076a1c4f0aaca8a4bc7b1f61ea
Ed Gonzalez <Edward.M.Gonzalez@gmail.com>
2015-09-13 13:02:27 -0400

    Initial Commit

---
+++ dldist/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  dldist
+//
+//  Created by Ed Gonzalez on 9/13/15.
+//  Copyright (c) 2015 Ed Gonzalez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
